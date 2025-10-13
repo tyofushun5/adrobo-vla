42645d4695518597b62c9f96ec3eb6e903d5f388
shun <zenshun10@gmail.com>
2025-10-14 01:03:25 +0900

    10/14 commit

--- urdf_test.py
+++ urdf_test.py
@@ -4,7 +4,7 @@ import mujoco
 import mujoco.viewer
 
 
-model = mujoco.MjModel.from_xml_path("crane_x7_d435.urdf")
+model = mujoco.MjModel.from_xml_path("mjmodel.xml")
 data  = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
