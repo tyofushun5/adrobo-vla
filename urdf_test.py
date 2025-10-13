5c55b872479e4da127bf1edd25687df394df4e54
shun <zenshun10@gmail.com>
2025-10-13 23:05:44 +0900

    10/13 commit

---
+++ urdf_test.py
@@ -0,0 +1,17 @@
+import time
+
+import mujoco
+import mujoco.viewer
+
+
+model = mujoco.MjModel.from_xml_path("crane_x7_d435.urdf")
+data  = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    start = time.time()
+
+    while viewer.is_running():
+        step_start = time.time()
+        mujoco.mj_step(model, data)
+        viewer.sync()
+
