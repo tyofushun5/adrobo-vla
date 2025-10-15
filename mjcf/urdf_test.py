4bb55a532ac4aa5cbf34803b47d4e162a9c1a13b
shun <zenshun10@gmail.com>
2025-10-15 14:54:05 +0900

    10/15 commit 2

--- mjcf/urdf_test.py
+++ mjcf/urdf_test.py
@@ -1,10 +1,13 @@
 import time
+import os
 
 import mujoco
 import mujoco.viewer
 
+this_dir = os.path.dirname(os.path.abspath(__file__))
+xml_path = os.path.join(this_dir, "mjmodel.xml")
+model = mujoco.MjModel.from_xml_path(xml_path)
 
-model = mujoco.MjModel.from_xml_path("mjmodel.xml")
 data  = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
