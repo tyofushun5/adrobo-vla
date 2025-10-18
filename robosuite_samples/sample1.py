8e8bd0a1be3d52ea76a3cae65065a175e329694d
shun <zenshun10@gmail.com>
2025-10-18 22:02:48 +0900

    10/18 commit 2

---
+++ robosuite_samples/sample1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import robosuite as suite
+
+
+env = suite.make(
+    env_name="Lift",
+    robots="Panda",
+    has_renderer=True,
+    has_offscreen_renderer=False,
+    use_camera_obs=False,
+)
+
+# reset the environment
+env.reset()
+
+for i in range(1000):
+    action = np.random.randn(*env.action_spec[0].shape) * 0.1
+    obs, reward, done, info = env.step(action)
+    env.render()
